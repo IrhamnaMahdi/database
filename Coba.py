a0a06d562b24587d152091bf812809a56f289c1a
IrhamnaMahdi <161599074+IrhamnaMahdi@users.noreply.github.com>
2025-06-19 14:15:07 +0700

    Add files via upload

---
+++ Coba.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pandas as pd
+import psycopg2
+
+# Function to load data from PostgreSQL
+def get_db_connection():
+    return psycopg2.connect(
+        dbname="Review",
+        user="postgres",
+        password="tytydkuda",
+        host="localhost",
+        port=5432
+    )
+
+if "page" not in st.session_state:
+    st.session_state.page = "home"  # Default page
+
+def switch_page(page_name):
+    st.session_state.page = page_name
+    st.rerun()
+
+st.title("Review Kunjungan BPS KOTA JAKARTA UTARA")
+
+if st.session_state.page == "home":
+    def insert_data(nama_pengunjung, instansi, tujuan, respon_pengunjung):
+        with get_db_connection() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute('''
+                    INSERT INTO "kunjungan" ("nama_pengunjung", "instansi", "tujuan", "respon_pengunjung")
+                    VALUES (%s, %s, %s, %s);
+                ''', (nama_pengunjung, instansi, tujuan, respon_pengunjung))
+                connection.commit()
+    
+    st.title("REVIEW KUNJUNGAN")
+    nama_pengunjung = st.text_input("Nama Pengunjung")
+    instansi = st.text_input("Instansi Asal")
+    tujuan = st.text_input("Tujuan")
+    respon_pengunjung = st.radio("Bagaimana Kunjungan Anda:", ("☹️ Tidak Puas", "🙂 Puas", "😁 Sangat Puas"), horizontal=True)
+
+    if st.button("Kirim"):
+        if nama_pengunjung and instansi and tujuan and respon_pengunjung is not None:
+            insert_data(nama_pengunjung, instansi, tujuan, respon_pengunjung)
+            st.success(f"Terima Kasih {nama_pengunjung}, Respon anda sudah kami terima 😊")
+        else:
+            st.error("Harap Mengisi Seluruh Kolom Dengan Benar!")
+    if st.button("Liat Riwayat Kunjunagan"):
+        switch_page("data")
+
+elif st.session_state.page == "data":
+    def load_data():
+        with get_db_connection() as connection:
+            query = 'SELECT * FROM "kunjungan"'  # Adjust the query as needed
+            df = pd.read_sql(query, connection)
+        return df
+
+    data = load_data()
+    st.dataframe(data)
+    if st.button("Back"):
+        switch_page("home")
+
